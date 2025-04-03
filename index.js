066c658738689c997b4d45bd90f59ee27d2ee898
Simon <simonwarui93@gmail.com>
2025-04-03 11:42:18 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json()) // Convert response to JSON
+    .then(data => {
+        // Append the new id to the DOM
+        document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+        // Handle errors and append the message to the DOM
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
